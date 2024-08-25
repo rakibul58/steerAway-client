a99cf384014a74ae81682d27ea9b4c91f7758c04
rakibul58 <rhrahi14@gmail.com>
2024-08-25 21:49:39 +0600

    redux login setup

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,75 @@
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  DefinitionType,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+import { logout, setUser } from "../features/auth/authSlice";
+// import { toast } from "sonner";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${import.meta.env.VITE_SERVER_API}/api/v1`,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 404) {
+    console.log(result);
+  }
+  if (result?.error?.status === 403) {
+    console.log(result);
+  }
+
+  if (result?.error?.status === 401) {
+
+    const res = await fetch(`${import.meta.env.VITE_SERVER_API}/api/v1/auth/refresh-token`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
+};
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: baseQueryWithRefreshToken,
+  tagTypes: [],
+  endpoints: () => ({}),
+});
